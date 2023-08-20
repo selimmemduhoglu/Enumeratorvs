a234440e9aa61eae5797e104fc2e35da5bd5a274
selimmemduhoglu <mselimmemduhoglu@gmail.com>
2023-08-20 22:06:03 +0300

    Overloading

---
+++ Overloading/Program.cs
@@ -0,0 +1,33 @@
+﻿Student student = new()
+{
+    Name = "Selim",
+};
+
+Book book = new Book()
+{
+    Name= "Asil Kan",
+    Author= "Kazım Yurdakul",
+};
+
+//var s = student + book; // Bu şekilde işlem yapamayız o yüzden overloading yapmamız gerekiyor ki + opoeratörüyle 2 nesne arasında istediğimiz işlemi yapsın çünkü .n3t te böyle bir özellik yok.
+
+var s = student + book;
+class Student
+{
+    public string Name { get; set; }
+    public List<Book> Books { get; set; }
+
+
+}
+class Book
+{
+    public string Name { get; set; }
+    public string Author { get; set; }
+    public static Student operator +(Student s, Book b)
+    {
+        s.Books.Add(b);
+        return s;
+    }
+
+
+}
